948163976c64daa4bc08a30c7e5c4164362ac684
RonGokhle <cips173@gmail.com>
2012-08-20 05:17:43 +0530

    fix
    Change-Id: Ia065fbc85d5cc231abaa76c0ab2564f598201ec6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,8 @@ BOARD_KERNEL_CMDLINE := mem=215M console=ttyMSM2,115200n8 androidboot.hardware=p
 BOARD_KERNEL_BASE := 0x02808000
 BOARD_PAGE_SIZE := 0x00000800
 
+BOARD_NO_RGBX_8888 := true
+
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00440000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
@@ -22,5 +24,9 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_PREBUILT_KERNEL := device/lge/p350/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_LDPI_RECOVERY := true
+BOARD_HAS_JANKY_BACKBUFFER := true
+BOARD_CUSTOM_GRAPHICS           := ../../../device/lge/p350/recovery/graphics.c
+
 # Use this flag if the board has a ext4 partition larger than 2gb
 #BOARD_HAS_LARGE_FILESYSTEM := true
